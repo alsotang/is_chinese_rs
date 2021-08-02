1a798dfbfd69ecb95a2033b6633e11a3f7e1a102
IWANABETHATGUY <974153916@qq.com>
2021-08-02 15:53:44 +0800

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,7 @@
 //! assert!(!is_chinese::is_chinese("ss"));
 //! ```
 //!
-const CHINESE_SINGLE_CHAR: [u32; 19] = [
+const CHINESE_SINGLE_CHAR: [u32; 20] = [
     0xff0c, //，
     0x3002, //。
     0x00b7, //·
@@ -30,22 +30,22 @@ const CHINESE_SINGLE_CHAR: [u32; 19] = [
     0xff1a, //：
     0xff1b, //；
     0xff1f, //？
+    0xff1f, //HACK: ？ this duplicate for fit simd lane
 ];
 // [2018, 2019, 3300, 33ff, 3400, 4dbf, 4e00, 9fff, f900, faff, fe30, fe4f, 20000, 2a6df, 2a700, 2b73f, 2b740, 2b81f, 2b820, 2ceaf, 2f800, 2fa1f]
 pub const CHINESE_RANGE: [u32; 24] = [
     // sequence is determine by occurrence probability
-    0x2018, 0x2019, //‘- ’
-    0x201c, 0x201d,//“, //”
-    0x3300, 0x33ff, // https://en.wikipedia.org/wiki/CJK_Compatibility
-    0x3400, 0x4dbf, // CJK Unified Ideographs Extension A
     0x4e00, 0x9fff, // CJK Unified Ideographs
-    0xf900, 0xfaff, // CJK Compatibility Ideographs
-    // 0xf900, 0xfaff, // https://en.wikipedia.org/wiki/CJK_Compatibility_Ideographs
-    0xfe30, 0xfe4f, // https://en.wikipedia.org/wiki/CJK_Compatibility_Forms
+    0x2018, 0x2019, //‘- ’
+    0x3400, 0x4dbf, // CJK Unified Ideographs Extension A
     0x20000, 0x2a6df, // CJK Unified Ideographs Extension B
     0x2a700, 0x2b73f, // CJK Unified Ideographs Extension C
     0x2b740, 0x2b81f, // CJK Unified Ideographs Extension D
     0x2b820, 0x2ceaf, // CJK Unified Ideographs Extension E
+    0xf900, 0xfaff, // CJK Compatibility Ideographs
+    0x3300, 0x33ff, // https://en.wikipedia.org/wiki/CJK_Compatibility
+    0xfe30, 0xfe4f, // https://en.wikipedia.org/wiki/CJK_Compatibility_Forms
+    0xf900, 0xfaff, // https://en.wikipedia.org/wiki/CJK_Compatibility_Ideographs
     0x2f800, 0x2fa1f, // https://en.wikipedia.org/wiki/CJK_Compatibility_Ideographs_Supplement
 ];
 
@@ -62,15 +62,18 @@ pub fn is_chinese(str: &str) -> bool {
 
     use packed_simd::u32x4;
     let is_all_chinese = str.chars().all(|c| {
-        CHINESE_RANGE
+        let char_vec = u32x4::splat(c as u32);
+        CHINESE_SINGLE_CHAR
             .chunks(4)
             .map(u32x4::from_slice_unaligned)
-            .any(|slice| {
-                let vector = u32x4::splat(c as u32);
-                let compare = vector.ge(slice);
-                let mask = compare.bitmask();
-                mask == 1 || mask == 7
-            })  || CHINESE_SINGLE_CHAR.contains(&(c as u32)) 
+            .any(|char_vec| char_vec.eq(char_vec).any())
+            || CHINESE_RANGE
+                .chunks(4)
+                .map(u32x4::from_slice_unaligned)
+                .any(|slice| {
+                    let mask = char_vec.ge(slice).bitmask();
+                    mask == 1 || mask == 7
+                })
     });
 
     is_all_chinese
