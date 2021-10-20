ce03f29bf6e8406c53c2b1faf992e8fa0d759770
IWANABETHATGUY <974153916@qq.com>
2021-10-21 00:03:03 +0800

    perf: ⚡️ condition compile

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,7 @@
 //!
 //!
 //!
+
 ///
 /// ```
 /// assert!(is_chinese::is_chinese("中国"));
@@ -25,19 +26,89 @@ pub fn is_chinese(string: &str) -> bool {
     string.chars().all(is_chinese_char)
 }
 
-
 fn is_printable_ascii(string: &str) -> bool {
-    // println!("{}", string.len() % 16);
-    use packed_simd::u8x16;
-    let mask = u8x16::splat(127);
     let bytes = string.as_bytes();
-    bytes.chunks_exact(16).map(u8x16::from_slice_unaligned).any(|v| {
-        let or = mask | v;
-        let res = or.eq(mask).any();
-        res
-    }) | &bytes[string.len() - string.len() % 16..].iter().any(|ch| *ch <= 127)
+    (unsafe {
+        #[cfg(target_feature = "avx2")]
+        {
+            is_printable_ascii_avx2(bytes)
+        }
+        #[cfg(target_feature = "sse2")]
+        #[cfg(not(target_feature = "avx2"))]
+        {
+            is_printable_ascii_sse2(bytes)
+        }
+        #[cfg(not(target_feature = "sse2"))]
+        {
+            is_printable_ascii_fallback(bytes)
+        }
+    }) || has_printable_ascii_in_rest_slice(bytes, string)
 }
 
+fn has_printable_ascii_in_rest_slice(bytes: &[u8], string: &str) -> bool {
+    use packed_simd::{u8x16, u8x4, u8x8};
+
+    #[cfg(target_feature = "avx2")]
+    let lane = 32;
+    #[cfg(target_feature = "sse2")]
+    let lane = 16;
+    #[cfg(not(target_feature = "sse2"))]
+    let lane = 0;
+
+    let reminder = bytes.len() % lane;
+
+    #[cfg(target_feature = "avx2")]
+    match reminder {
+        16 => u8x16::from_slice_unaligned(&bytes[bytes.len() - 8..])
+            .le(u8x16::splat(127))
+            .any(),
+        8 => u8x8::from_slice_unaligned(&bytes[bytes.len() - 8..])
+            .le(u8x8::splat(127))
+            .any(),
+        4 => u8x4::from_slice_unaligned(&bytes[bytes.len() - 8..])
+            .le(u8x4::splat(127))
+            .any(),
+        _ => bytes[string.len() - reminder..].iter().any(|ch| *ch <= 127),
+    }
+    #[cfg(target_feature = "sse2")]
+    #[cfg(not(target_feature = "avx2"))]
+    match reminder {
+        8 => u8x8::from_slice_unaligned(&bytes[bytes.len() - 8..])
+            .le(u8x8::splat(127))
+            .any(),
+        4 => u8x4::from_slice_unaligned(&bytes[bytes.len() - 8..])
+            .le(u8x4::splat(127))
+            .any(),
+        _ => bytes[string.len() - reminder..].iter().any(|ch| *ch <= 127),
+    }
+    #[cfg(not(target_feature = "sse2"))]
+    bytes[string.len() - reminder..].iter().any(|ch| *ch <= 127)
+    
+}
+
+#[target_feature(enable = "sse2")]
+unsafe fn is_printable_ascii_sse2(bytes: &[u8]) -> bool {
+    use packed_simd::u8x16;
+    let mask = u8x16::splat(127);
+    bytes
+        .chunks_exact(16)
+        .map(u8x16::from_slice_unaligned)
+        .any(|v| v.le(mask).any())
+}
+
+#[target_feature(enable = "avx2")]
+unsafe fn is_printable_ascii_avx2(bytes: &[u8]) -> bool {
+    use packed_simd::u8x32;
+    let mask = u8x32::splat(127);
+    bytes
+        .chunks_exact(32)
+        .map(u8x32::from_slice_unaligned)
+        .any(|v| v.le(mask).any())
+}
+
+fn is_printable_ascii_fallback(bytes: &[u8]) -> bool {
+    bytes.iter().any(|v| *v <= 127)
+}
 #[inline]
 fn is_chinese_char(ch: char) -> bool {
     match ch as u32 {
